e9948bf5ce5e15d8793b8274921d2d01c550dbc2
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2025-06-28 22:15:16 +0600

    delete and single user get api added

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,6 +23,16 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+const getUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: error.mesaage });
+  }
+};
+
 const updateUser = async (req, res) => {
   const id = await req.params.id;
   const { name, email } = req.body;
@@ -39,4 +49,14 @@ const updateUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser, getAllUsers, updateUser };
+const deleteUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await db.query("DELETE FROM users WHERE id = $1", [id]);
+    res.status(200).json({ message: "Deleted!" });
+  } catch (error) {
+    res.status(500).json({ error: err.mesaage });
+  }
+};
+
+module.exports = { createUser, getAllUsers, updateUser, deleteUser, getUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,15 @@ const {
   createUser,
   getAllUsers,
   updateUser,
+  deleteUser,
+  getUser,
 } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/", createUser);
 router.get("/", getAllUsers);
+router.get("/:id", getUser);
 router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
